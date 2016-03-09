79b171a1680dbcb65511c8d5fb07ec629dbadb9d
Carl Whittaker <carl359@gmail.com>
2016-03-09 17:55:48 +0000

    Set a more sensible default speed for the orientation controls

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import THREE from 'three'; // Ensure that three is imported
+window.THREE = THREE;
 import Gitlactica from './lib/gitlactica';
 const gitlactica = new Gitlactica(document.body);
 gitlactica.start();
